2f7eb6d000ca71020d7c28698195cb25bd4ab1d9
kurokobo <2920259+kurokobo@users.noreply.github.com>
2019-12-26 16:33:45 +0000

    Fix environment variable

--- rest/rest_to_mongodb.py
+++ rest/rest_to_mongodb.py
@@ -56,4 +56,4 @@ def receive_data():
 
 if __name__ == "__main__":
     db = setup_db()
-    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", "5100")))
+    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", "8080")))
